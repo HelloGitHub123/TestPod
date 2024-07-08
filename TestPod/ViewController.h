27f22943e684d4077e8d884e49f871d948f22762
李标 <li.biao3@iwhalecloud.com>
2024-07-08 16:06:58 +0800

    init

---
+++ TestPod/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BOL
+//
+//  Created by 李标 on 2024/7/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestPod/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  BOL
+//
+//  Created by 李标 on 2024/7/5.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
